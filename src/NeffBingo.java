e47bad87f32177db16735544e464fe413de2e115
devo149 <devo149@dad356f7-bf8a-93d5-e511-eabc81cb935a>
2011-04-01 23:21:56 +0000

    Initial configuration.

---
+++ src/NeffBingo.java
@@ -0,0 +1,107 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ * Creates a Bingo board containing
+ * quotes often spoken by Brother Neff.
+ *
+ * @author tblyons, domand, har3000
+ */
+public class NeffBingo
+   implements ActionListener
+{
+   QuoteGen mQuoteGen;
+   JFrame mBoard;
+   JPanel mPanel;
+   JToggleButton[] tButton = new JToggleButton[25];
+
+   /**
+    * @param args the command line arguments
+    */
+   public static void main(String[] args)
+   {
+      NeffBingo game = new NeffBingo();
+      game.play();
+   }
+
+   public void play()
+   {
+      mQuoteGen = new QuoteGen();
+      mBoard = new JFrame();
+      mPanel = new JPanel();
+      mPanel.setLayout(new GridLayout(5,5));
+
+      for (int i = 0; i < 25; i++)
+      {
+         String tempStr;
+         if (i != 12)
+         {
+            tButton[i] = new JToggleButton("<html>" + mQuoteGen.getQuote() +
+                                           "</html>");
+
+            tButton[i].setForeground(Color.BLACK);
+         }
+         else
+         {
+            // this is the center square, and is generally a free space
+            tButton[i] = new JToggleButton("<html>*Free Space*</html>", true);
+            tButton[i].setForeground(Color.BLACK);
+            tButton[i].setEnabled(false);
+         }
+         tButton[i].addActionListener(this);
+         mPanel.add(tButton[i]);
+      }
+
+      mBoard.getContentPane().add(mPanel);
+      mBoard.setSize(500,500);
+      mBoard.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      mBoard.setVisible(true);
+   }
+
+   public boolean isWin()
+   {
+      if (tButton[0].isSelected() && tButton[6].isSelected() && tButton[12].isSelected() &&
+          tButton[18].isSelected() && tButton[24].isSelected() ||
+          (tButton[20].isSelected() && tButton[16].isSelected() && tButton[12].isSelected() &&
+           tButton[8].isSelected() && tButton[4].isSelected()))
+      {
+         //finished diagonal
+         return true;
+      }
+
+      for (int i = 0; i < 5; i++)
+      {
+         if (tButton[5*i].isSelected() && tButton[5*i + 1].isSelected() && tButton[5*i + 2].isSelected() &&
+             tButton[5*i + 3].isSelected() && tButton[5*i + 4].isSelected())
+         {
+            //finished row
+            return true;
+         }
+         else if (tButton[i].isSelected() && tButton[i+5].isSelected() && tButton[i+10].isSelected() &&
+                  tButton[i+15].isSelected() && tButton[i+20].isSelected())
+         {
+            //finsihed column
+            return true;
+         }
+      }
+
+      return false;
+   }
+
+   public void actionPerformed(ActionEvent event)
+   {
+      JToggleButton button = (JToggleButton) event.getSource();
+
+      if (isWin())
+      {
+         tButton[12].setBackground(Color.RED);
+         tButton[12].setText("BINGO!");
+      }
+      else
+      {
+         tButton[12].setForeground(Color.BLACK);
+         tButton[12].setText("<html>*Free Space*</html>");
+      }
+   }
+}
---
+++ src/QuoteGen.java
@@ -0,0 +1,87 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.util.Collection;
+import java.util.ArrayList;
+
+/**
+ * <code>QuoteGen</code> reads the Quote file
+ * and pulls out quotations to label the bingo
+ * grid with.
+ *
+ * @author Devin Doman
+ */
+public class QuoteGen
+{
+   private Collection<String> mQuotes;
+
+   /**
+    * QuoteGen Constructor
+    */
+   public QuoteGen()
+   {
+      mQuotes = new ArrayList<String>();
+      readQuotes("Quotes.txt");
+   }
+
+   /**
+    * Retrieves the quotes to be generated from a file
+    *
+    * @param pFile The file location of the quotations
+    */
+   private void readQuotes(String pFile)
+   {
+      BufferedReader in = null;
+      try
+      {
+         in = new BufferedReader(new FileReader(pFile));
+
+         //Read the data from the file and place in mQuotes
+         while (in.ready())
+         {
+            String line = in.readLine().trim();
+
+            //do not put in a blank line
+            if (!line.isEmpty())
+            {
+               mQuotes.add(line);
+            }
+         }
+      }
+      catch (FileNotFoundException e)
+      {
+         System.out.println("File not found.");
+      }
+      catch (IOException e)
+      {
+         System.out.println("Error reading data.");
+      }
+   }
+
+   /**
+    * Pull out a quote from the collection
+    *
+    * @return a quote from the collection
+    *         null if the collection is empty
+    */
+   public String getQuote()
+   {
+      String rQuote = null;
+      int rand;
+      ArrayList<String> quotes;
+
+      if (!mQuotes.isEmpty())
+      {
+         quotes = (ArrayList<String>) mQuotes;
+         //pick a random number to get a quote with
+         rand = (int) (Math.random() * quotes.size());
+
+         rQuote = quotes.get(rand);
+
+         //do not use this quote again
+         mQuotes.remove(rQuote);
+      }
+      return rQuote;
+   }
+}
