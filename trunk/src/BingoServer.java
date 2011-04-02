1be9a16e36a2e53754a6af582c91ad57d5e5e4d4
devo149 <devo149@dad356f7-bf8a-93d5-e511-eabc81cb935a>
2011-04-02 22:45:31 +0000

    Made the Server into a GUI

--- trunk/src/BingoServer.java
+++ trunk/src/BingoServer.java
@@ -2,13 +2,30 @@
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 /**
  *
  * @author Devin Doman
  */
 public class BingoServer
+   extends JFrame
 {
+   private JLabel mMessage;
+   
+   public BingoServer()
+   {
+      super("Bingo Server");
+
+      mMessage = new JLabel("   empty");
+      add(mMessage);
+      setSize(240, 100);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setVisible(true);
+   }
+
+
    /**
     * Starting place for the BingoServer
     *
@@ -16,37 +33,41 @@ public class BingoServer
     * @throws Exception
     */
    public static void main(String argv[])
-      throws Exception
    {
-      //Set the port number
-      int port = 1352;
+      new BingoServer().run();
+   }
 
-      //Establish the listen socket.
-      ServerSocket welcomeSocket = new ServerSocket(port);
-
+   public void run()
+   {
       try
       {
+         //Set the port number
+         int port = 1352;
+
+         //Establish the listen socket.
+         ServerSocket welcomeSocket = new ServerSocket(port);
+
+
          InetAddress addr = InetAddress.getLocalHost();
          String ip = addr.getHostAddress();
 
-         System.out.println("Address: " + ip);
-      }
-      catch(Exception e)
-      {
+         mMessage.setText("   Server IP Address: " + ip);
 
+         //Process service requests in an infinite loop
+         while (true)
+         {
+            //Listen for a TCP connection request
+            Socket connectionSocket = welcomeSocket.accept();
+            // Construct an object to process the request.
+            BingoHandler request = new BingoHandler(connectionSocket);
+            // Create a new thread to process the request.
+            Thread thread = new Thread(request);
+            // Start the thread.
+            thread.start();
+         }
       }
-
-      //Process service requests in an infinite loop
-      while (true)
+      catch (Exception e)
       {
-         //Listen for a TCP connection request
-         Socket connectionSocket = welcomeSocket.accept();
-         // Construct an object to process the request.
-         BingoHandler request = new BingoHandler(connectionSocket);
-         // Create a new thread to process the request.
-         Thread thread = new Thread(request);
-         // Start the thread.
-         thread.start();
       }
    }
 }
